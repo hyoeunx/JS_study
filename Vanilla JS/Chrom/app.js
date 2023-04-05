2d61adab577ccbc1f04e80b8ebbb6c9c4f018dd5
hyoeunx <shimhe.06@gmail.com>
2023-04-05 23:21:33 +0900

    0405

--- Vanilla JS/Chrom/app.js
+++ Vanilla JS/Chrom/app.js
@@ -1,3 +1,7 @@
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 
-console.log(title);
+// console.log(title);
+
+const title = document.querySelector(".hello:first-child h1");
+
+title.innerText = "Hello";
